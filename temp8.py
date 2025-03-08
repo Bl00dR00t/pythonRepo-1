1a694c3f5f935f04d77dc4d54aa98dc95696c738
Bl00dR00t <helloww199@gmail.com>
2025-03-08 23:44:16 +0530

    Add files via upload

---
+++ temp8.py
@@ -0,0 +1,18 @@
+import requests
+
+baseUrl = "https://api.batmanapi.com/v1"
+
+def aFunctionThatFetchesAfuckingApi(MakeAfuckingChoice):
+    apifetch = f"{baseUrl}/{MakeAfuckingChoice}"
+    requestNow = requests.get(apifetch)
+    
+    if requestNow.status_code == 200:  # Corrected this line
+        makereq = requestNow.json()
+        return makereq
+    else:
+        print("error")
+
+charChoice = 'characters'
+variableThatreturnsFun = aFunctionThatFetchesAfuckingApi(charChoice)
+
+print(variableThatreturnsFun)
